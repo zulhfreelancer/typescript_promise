24bf7b32013569c174d42fee6b3e727391221345
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2020-06-11 17:13:53 +0800

    Improve indentation

--- index.ts
+++ index.ts
@@ -2,46 +2,46 @@ import { createReadStream } from "fs";
 import { parse } from "papaparse";
 
 class Greeter {
-	greeting: string;
-	constructor(message: string) {
-		this.greeting = message;
+  greeting: string;
+  constructor(message: string) {
+    this.greeting = message;
 
-		// This will get printed first before `greet()`
-		const name = "John";
-		console.log(name);
+    // This will get printed first before `greet()`
+    const name = "John";
+    console.log(name);
 
-		const dataProvider = function (filepath) {
-			const file = createReadStream(filepath);
-			return new Promise((resolve, reject) => {
-				parse(file, {
-					header: true,
-					complete(results, file) {
-						// To simulate async processing (reading file)
-						setTimeout(() => {
-							resolve(results.data);
-						}, 3000);
-					},
-					error(err, file) {
-						reject(err);
-					},
-				});
-			});
-		};
+    const dataProvider = function (filepath) {
+      const file = createReadStream(filepath);
+      return new Promise((resolve, reject) => {
+        parse(file, {
+          header: true,
+          complete(results, file) {
+            // To simulate async processing (reading file)
+            setTimeout(() => {
+              resolve(results.data);
+            }, 3000);
+          },
+          error(err, file) {
+            reject(err);
+          },
+        });
+      });
+    };
 
-		const dataConsumer = async function () {
-			try {
-				const data = await dataProvider("./people.csv");
-				console.log(data);
-				console.log(typeof data);
-			} catch (err) {
-				console.error("Could not parse json", err);
-			}
-		};
-		dataConsumer();
-	}
-	greet() {
-		return "Hello, " + this.greeting;
-	}
+    const dataConsumer = async function () {
+      try {
+        const data = await dataProvider("./people.csv");
+        console.log(data);
+        console.log(typeof data);
+      } catch (err) {
+        console.error("Could not parse json", err);
+      }
+    };
+    dataConsumer();
+  }
+  greet() {
+    return "Hello, " + this.greeting;
+  }
 }
 
 let greeter = new Greeter("world");
